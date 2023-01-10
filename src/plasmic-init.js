d47a8a0250af9554b71db4dedffe05d5e7ac7571
Zach Spriggs <zachspriggs@gmail.com>
2023-01-10 13:52:33 -0800

    updated project and path ID for plasmic

--- src/plasmic-init.js
+++ src/plasmic-init.js
@@ -3,8 +3,10 @@ import { initPlasmicLoader } from "@plasmicapp/loader-vue";
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: process.env.VUE_APP_NADI_PLASMIC_PROJECT_ID,   // ID of a project you are using
-      token: process.env.VUE_APP_PLASMIC_TOKEN,  // API token for that project
+      id: "7Vf8ccS5NVpi2p94xhvgdE",   // ID of a project you are using
+      token: "030jwm8EMhBdwUGFDt7NSdtd6f6dLsC7ZNvKvHqzZTZSu43JXeDmmORT4CylHfyLUf1MghMUYE5ZXkCK35g", 
+      // id: process.env.VUE_APP_NADI_PLASMIC_PROJECT_ID,   // ID of a project you are using
+      // token: process.env.VUE_APP_PLASMIC_TOKEN,  // API token for that project
     }
   ],
   // Fetches the latest revisions, whether or not they were unpublished!
